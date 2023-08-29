5fed50a926b27493f26bac5772e6e5084893576f
Vhmit <viktor.9630@hotmail.com>
2023-08-28 21:12:53 -0300

    j4primelte: Configure architecture and platform flags
    * Initial 64 bits
    
    Change-Id: Ic03293b716f79a3b40d831ac1ca5cde8199ba64b

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -10,8 +10,31 @@ TARGET_VENDOR := samsung
 # Inherit from common msm8917-common
 -include device/samsung/msm8917-common/BoardConfigCommon.mk
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := cortex-a53
+TARGET_CPU_CORTEX_A53 := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a53
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := j4primelte, j4primeltedx
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := RG13A002KU
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+BOARD_USES_QCOM_HARDWARE := true
+TARGET_BOARD_PLATFORM := msm8937
 
 # Inherit from the proprietary version
 -include vendor/samsung/j4primelte/BoardConfigVendor.mk
