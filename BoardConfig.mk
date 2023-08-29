ccda2bfbde94a5e68158862b4a40b3a03351ed62
Vhmit <viktor.9630@hotmail.com>
2023-08-28 21:07:55 -0300

    j4primelte: Inherit propritary files
    Change-Id: I7f9d5c7c7a11427f0131490276f0bc90ac676a08

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,5 +7,11 @@
 DEVICE_PATH := device/samsung/j4primelte
 TARGET_VENDOR := samsung
 
+# Inherit from common msm8917-common
+-include device/samsung/msm8917-common/BoardConfigCommon.mk
+
 # Assert
 TARGET_OTA_ASSERT_DEVICE := j4primelte, j4primeltedx
+
+# Inherit from the proprietary version
+-include vendor/samsung/j4primelte/BoardConfigVendor.mk
